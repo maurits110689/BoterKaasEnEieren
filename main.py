30831fe5a3e1acdcc915e0cc381d38ad7a4ff021
Maurits van <74552251+maurits110689@users.noreply.github.com>
2022-09-19 14:22:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from bke import MLAgent, is_winner, opponent, start
+ 
+ 
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+my_agent = MyAgent()
+start(player_x=my_agent)
